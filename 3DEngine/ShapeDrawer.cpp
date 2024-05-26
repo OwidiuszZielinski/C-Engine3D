25e685beb9e66e2c9f13657c2629dcc597888dd7
OwidiuszZielinski <zielinski.owidiusz@gmail.com>
2024-05-26 16:52:32 +0200

    add

--- 3DEngine/ShapeDrawer.cpp
+++ 3DEngine/ShapeDrawer.cpp
@@ -28,16 +28,82 @@ void ShapeDrawer::drawCone(float base, float height, int slices, int stacks) {
     glutSolidCone(base, height, slices, stacks);
 }
 void ShapeDrawer::drawTriangle(float size) {
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0, 0.0, 0.0); // Wierzcho³ek 1: czerwony
-    glVertex3f(-1.0, -1.0, 0.0);
+    //glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+    //   // Top face (y = 1.0f)
+    //   // Define vertices in counter-clockwise (CCW) order with normal pointing out
+    //glColor3f(0.0f, 1.0f, 0.0f);     // Green
+    //glVertex3f(1.0f, 1.0f, -1.0f);
+    //glVertex3f(-1.0f, 1.0f, -1.0f);
+    //glVertex3f(-1.0f, 1.0f, 1.0f);
+    //glVertex3f(1.0f, 1.0f, 1.0f);
 
-    glColor3f(0.0, 1.0, 0.0); // Wierzcho³ek 2: zielony
-    glVertex3f(1.0, -1.0, 0.0);
+    //// Bottom face (y = -1.0f)
+    //glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+    //glVertex3f(1.0f, -1.0f, 1.0f);
+    //glVertex3f(-1.0f, -1.0f, 1.0f);
+    //glVertex3f(-1.0f, -1.0f, -1.0f);
+    //glVertex3f(1.0f, -1.0f, -1.0f);
 
-    glColor3f(0.0, 0.0, 1.0); // Wierzcho³ek 3: niebieski
-    glVertex3f(0.0, 1.0, 0.0);
-    glEnd();
+    //// Front face  (z = 1.0f)
+    //glColor3f(1.0f, 0.0f, 0.0f);     // Red
+    //glVertex3f(1.0f, 1.0f, 1.0f);
+    //glVertex3f(-1.0f, 1.0f, 1.0f);
+    //glVertex3f(-1.0f, -1.0f, 1.0f);
+    //glVertex3f(1.0f, -1.0f, 1.0f);
+
+    //// Back face (z = -1.0f)
+    //glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+    //glVertex3f(1.0f, -1.0f, -1.0f);
+    //glVertex3f(-1.0f, -1.0f, -1.0f);
+    //glVertex3f(-1.0f, 1.0f, -1.0f);
+    //glVertex3f(1.0f, 1.0f, -1.0f);
+
+    //// Left face (x = -1.0f)
+    //glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+    //glVertex3f(-1.0f, 1.0f, 1.0f);
+    //glVertex3f(-1.0f, 1.0f, -1.0f);
+    //glVertex3f(-1.0f, -1.0f, -1.0f);
+    //glVertex3f(-1.0f, -1.0f, 1.0f);
+
+    //// Right face (x = 1.0f)
+    //glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+    //glVertex3f(1.0f, 1.0f, -1.0f);
+    //glVertex3f(1.0f, 1.0f, 1.0f);
+    //glVertex3f(1.0f, -1.0f, 1.0f);
+    //glVertex3f(1.0f, -1.0f, -1.0f);
+    //glEnd();  // End of drawi
+
+    glBegin(GL_TRIANGLES);           // Begin drawing the pyramid with 4 triangles
+      // Front
+    glColor3f(1.0f, 0.0f, 0.0f);     // Red
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+
+    // Right
+    glColor3f(1.0f, 0.0f, 0.0f);     // Red
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    
+    glVertex3f(1.0f, -1.0f, 1.0f);
+   
+    glVertex3f(1.0f, -1.0f, -1.0f);
+
+    // Back
+    glColor3f(1.0f, 0.0f, 0.0f);     // Red
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+
+    // Left
+    glColor3f(1.0f, 0.0f, 0.0f);       // Red
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+   
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glEnd();   // Done drawing the pyramid
 }
 
 
